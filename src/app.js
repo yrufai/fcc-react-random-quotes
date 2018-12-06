7d6048f8a07849c4b56f20eb35a24fe3482bd7d3
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-06 23:40:28 +0000

    normalise.css removed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
-import "../node_modules/normalize.css/normalize.css";
 import "./styles/style.scss";
 
 
