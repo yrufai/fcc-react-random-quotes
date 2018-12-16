2bf4750a731490f6b7e80d1ffa40bad7059bdce9
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-16 22:08:58 +0000

    touched

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,34 @@
 import React from "react";
 
 class Home extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            quotes : ["red", "yellow", "blue","green","purple", "pink"],
-            colors : ["red", "yellow", "blue","green","purple", "pink"]
+            quotes: ["red", "yellow", "blue", "green", "purple", "pink"],
+            colors: "",
         };
-        this.nextQuote = this.nextQuote.bind(this); 
+        this.nextQuote = this.nextQuote.bind(this);
         this.getColors = this.getColors.bind(this);
     }
-    getColors(){
-        const randomColors = this.state.colors[Math.floor(Math.random()* this.state.colors.length)]
+    getColors() {
+        const randomColors = this.state.colors[Math.floor(Math.random() * this.state.colors.length)]
         return randomColors;
     }
     // moves to display next quotes 
-    nextQuote(){  
+    nextQuote() {
         const random = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
-
+        console.log("receiving message from ", random);
+        this.setState({
+            colors: random
+        })
     }
-    render(){
+    render() {
         return (
             <div>
-                <p></p>
-                <button onClick={this.random}>New Quotes</button>
+                <h1>{this.state.quotes.map((quote, index)=>{
+                    return quote
+                }) }</h1>
+                <button onClick={this.nextQuote}>New Quotes</button>
             </div>
         )
     }
