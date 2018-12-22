a1dc8870b78e9dc3b02edb5765388a3ad027db97
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-22 17:21:08 +0000

    random quotes worked

--- src/app.js
+++ src/app.js
@@ -1,34 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.scss";
 
-
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            quotes: ["red", "yellow", "blue", "green", "purple", "pink"],
-            colors: [],
-        };
-        this.nextQuote = this.nextQuote.bind(this);
-    }
-    // moves to display next quotes 
-    nextQuote() {
-        const random = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
-        console.log("receiving ", random);
-        this.setState({
-            colors: random
-        })
-    }
-    render() {
-        return (
-            <div style={{backgroundColor: this.state.colors}}>
-                <h1>{this.state.colors}</h1>
-                <button onClick={this.nextQuote}>New Quotes</button>
-            </div>
-        )
-    }
-}
 
 ReactDOM.render(<Home />, document.getElementById("app")); 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,65 @@
 import React from "react";
+import { random } from "lodash";
+import { Button } from 'reactstrap';
+
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            colors: ["red", "yellow", "blue", "green", "purple", "pink"],
+            selectedQuotesIndex: null,
+            textColor: [],
+            quotes: [],
+            colors: ["red", "lightcoral", "lightcyan", "lime", "fern", "yellow", "blue", "green", "purple", "pink", "mediumturquoise", "palegreen"],
         };
         this.next = this.next.bind(this);
+        this.selectQuotesIndex = this.selectQuotesIndex.bind(this);
     }
     // moves to display next quotes 
     next() {
-        const {colors} = this.state
-        const random = colors [Math.floor(Math.random() * color.length)]
+        const { colors } = this.state
+        const random = colors[Math.floor(Math.random() * colors.length)]
         document.body.style.backgroundColor = random;
+        document.body.style.color=random;
+        let battons = document.querySelectorAll("Button");
+        battons.forEach((item)=>{
+            item.style.backgroundColor=random;
+        });
     }
-    
+    componentDidMount() {
+        fetch("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
+            .then(data => data.json())
+            .then(quotes => this.setState({ quotes }, () => {
+                this.setState({ selectedQuotesIndex: this.selectQuotesIndex() })
+            }))
+            .catch(error => console.log("something is wrong", error))
+    }
+
+    get selectedQuote() {
+        if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuotesIndex)) {
+            return
+        }
+        return this.state.quotes[this.state.selectedQuotesIndex];
+    }
+
+    selectQuotesIndex() {
+        if (this.state.quotes.length) {
+            return random(0, this.state.quotes.length - 1);
+        }
+        return undefined;
+    }
+   
     render() {
         return (
             <div>
-                <button onClick={this.next}>Next</button>
+                <div id="wrapper">
+                    <div id="quotes-box">
+                        <div className="quote-text" >{this.selectedQuote ? this.selectedQuote.quote : ""}</div>
+                        <div className="quote-author" >{this.selectedQuote ? this.selectedQuote.author : ""}</div>
+                        <Button className="btn btn-success btn-lg float-right" onClick={this.next}>Next</Button>
+                        <Button className="btn btn-success btn-lg float-left" >Github</Button>
+                    </div>
+                    <div className="footer"> by YAKUBU AHMED EL-RUFAI</div>
+                </div>
             </div>
         )
     }
