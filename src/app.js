c17eabdb0420f2558432ab204e544da600a005ff
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-23 13:03:41 +0000

    useless files removed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppRouter from "./routers/AppRouter";
 import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.scss";
