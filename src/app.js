ea8bf919b8f73165fd280ac678e48941722a48b5
yakubu rufai mahatma <yrufai23@gmail.com>
2018-12-23 12:24:50 +0000

    random quotes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import AppRouter from "./routers/AppRouter";
 import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.scss";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab, faQuoteLeft);
 
 
 ReactDOM.render(<Home />, document.getElementById("app")); 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,43 @@
 import React from "react";
 import { random } from "lodash";
 import { Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             selectedQuotesIndex: null,
-            textColor: [],
             quotes: [],
-            colors: ["red", "lightcoral", "lightcyan", "lime", "fern", "yellow", "blue", "green", "purple", "pink", "mediumturquoise", "palegreen"],
+            colors: ["#E52B50","#FFBF00","#9966CC","#FBCEB1","#89CFF0","#0095B6","#800020","#DE3163","#007BA7","#F88379","#50C878","#29AB87","#B57EDC","#003153","#EE82EE"]
         };
         this.next = this.next.bind(this);
         this.selectQuotesIndex = this.selectQuotesIndex.bind(this);
     }
     // moves to display next quotes 
+
+    componentDidMount() {
+        fetch("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
+            .then(data => data.json())
+            .then(quotes => this.setState({ quotes }, this.next))
+            .catch(error => console.log("something is wrong", error))
+    }
+
     next() {
         const { colors } = this.state
         const random = colors[Math.floor(Math.random() * colors.length)]
         document.body.style.backgroundColor = random;
-        document.body.style.color=random;
+        document.body.style.color = random;
         let battons = document.querySelectorAll("Button");
-        battons.forEach((item)=>{
-            item.style.backgroundColor=random;
+        battons.forEach((item) => {
+            item.style.backgroundColor = random;
         });
-    }
-    componentDidMount() {
-        fetch("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
-            .then(data => data.json())
-            .then(quotes => this.setState({ quotes }, () => {
-                this.setState({ selectedQuotesIndex: this.selectQuotesIndex() })
-            }))
-            .catch(error => console.log("something is wrong", error))
+        this.setState({
+            selectedQuotesIndex: this.selectQuotesIndex()
+        });
+
     }
 
     get selectedQuote() {
@@ -47,18 +53,18 @@ class Home extends React.Component {
         }
         return undefined;
     }
-   
+
     render() {
         return (
             <div>
                 <div id="wrapper">
                     <div id="quotes-box">
-                        <div className="quote-text" >{this.selectedQuote ? this.selectedQuote.quote : ""}</div>
+                        <div className="quote-text" ><FontAwesomeIcon icon={faQuoteLeft} size="lg" />{this.selectedQuote ? " "+this.selectedQuote.quote+" " : ""}<FontAwesomeIcon icon={faQuoteRight} size="lg" /></div>
                         <div className="quote-author" >{this.selectedQuote ? this.selectedQuote.author : ""}</div>
-                        <Button className="btn btn-success btn-lg float-right" onClick={this.next}>Next</Button>
-                        <Button className="btn btn-success btn-lg float-left" >Github</Button>
+                        <Button className="btn btn-success btn-lg float-right" onClick={this.next}>New Quotes</Button>
+                        <Button className="btn btn-success btn-lg float-left" ><FontAwesomeIcon icon={faGithub}  />{"  "} Github</Button>
                     </div>
-                    <div className="footer"> by YAKUBU AHMED EL-RUFAI</div>
+                    <div className="footer"> by Yakubu Ahmed El-rufai</div>
                 </div>
             </div>
         )
